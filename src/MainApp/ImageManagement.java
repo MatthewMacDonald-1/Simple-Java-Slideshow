ebf13377126b78d8db279945bd9908afde9c6a13
Raptor_854 <113599350+MatthewMacDonald-1@users.noreply.github.com>
2023-06-30 18:37:10 +1200

    Added basic functionality to ImageManagement

--- src/MainApp/ImageManagement.java
+++ src/MainApp/ImageManagement.java
@@ -1,32 +1,43 @@
 package MainApp;
 
 import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
 
 public class ImageManagement {
 
     /**
      * The image index used to return the image that is the current
      */
-    private int currentActiveImage = 0;
+    private int currentActiveImageIndex = -1;
+
+    private int currentCachedImageIndex = -1;
+    private File[] imageFiles = null;
 
     /**
      * Current images in memory
      */
-    private BufferedImage[] images = null;
+    private BufferedImage[] cachedImages = null;
 
     /**
-     * The default number of images for the program to keep in memory
+     * <p>The default number of images for the program to keep in memory.</p>
+     * <p>Prev, current, next</p>
      */
-    private static int DEFAULT_NUM_IMAGES = 2;
+    private final int DEFAULT_NUM_IMAGES = 3;
+    private boolean isUsingDefault = false;
+    private int numImages = 0;
 
     public ImageManagement() {
-        images = new BufferedImage[DEFAULT_NUM_IMAGES];
+        cachedImages = new BufferedImage[DEFAULT_NUM_IMAGES];
+        isUsingDefault = true;
     }
 
     public boolean hasImage() {
-        if (images != null) {
-            if (currentActiveImage >= images.length || currentActiveImage < 0) {
-                if (images[currentActiveImage] != null) {
+        if (cachedImages != null) {
+            if (currentActiveImageIndex >= cachedImages.length || currentActiveImageIndex < 0) {
+                if (cachedImages[currentActiveImageIndex] != null) {
                     return true;
                 }
             }
@@ -35,7 +46,50 @@ public class ImageManagement {
     }
 
     public BufferedImage getCurrent() {
-        return images[currentActiveImage];
+        return cachedImages[currentActiveImageIndex];
+    }
+
+    public boolean setImageFiles(File[] imageFiles) {
+        if (imageFiles.length >= 1) {
+            this.imageFiles = imageFiles;
+            if (imageFiles.length < DEFAULT_NUM_IMAGES) {
+                cachedImages = new BufferedImage[imageFiles.length];
+            } else {
+                cachedImages = new BufferedImage[DEFAULT_NUM_IMAGES];
+            }
+            prepFromStart();
+            return true;
+        }
+        return false;
+    }
+
+    private void prepFromStart() {
+        currentCachedImageIndex = 0;
+        currentActiveImageIndex = 0;
+        for (int i = 0; i < cachedImages.length && i < imageFiles.length; i++) {
+            try {
+                cachedImages[i] = ImageIO.read(imageFiles[i]);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public void goToNextImage() {
+        currentActiveImageIndex ++;
+        if (currentActiveImageIndex >= imageFiles.length) currentActiveImageIndex = 0;
+        currentCachedImageIndex ++;
+        if (currentCachedImageIndex >= cachedImages.length) currentCachedImageIndex = 0;
+
+        // load image 2 places ahead
+        int loadToIndex = (currentCachedImageIndex + 2 >= cachedImages.length ? currentCachedImageIndex + 2 - cachedImages.length : currentCachedImageIndex + 2);
+        int loadFromImageFilesIndex = (currentActiveImageIndex + 2 >= imageFiles.length ? currentActiveImageIndex + 2 - imageFiles.length : currentActiveImageIndex + 2);
+
+        try {
+            cachedImages[loadToIndex] = ImageIO.read(imageFiles[loadFromImageFilesIndex]);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
 }
