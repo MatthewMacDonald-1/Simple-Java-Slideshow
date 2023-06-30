c778d33ff6bc2ae1320ae9f61eab6c37de9b6a22
Raptor_854 <113599350+MatthewMacDonald-1@users.noreply.github.com>
2023-07-01 08:25:18 +1200

    Image now always fits bounds

--- src/MainApp/ImagePanel.java
+++ src/MainApp/ImagePanel.java
@@ -19,6 +19,8 @@ public class ImagePanel extends JPanel {
     public ImagePanel(ImageManagement imageManagement) throws IOException {
         noImage = ImageIO.read(ImagePanel.class.getClassLoader().getResource("no-image.png"));
         image = imageManagement;
+
+        //setBackground(Color.BLACK); // sets frame background to black
     }
 
     /**
@@ -42,25 +44,47 @@ public class ImagePanel extends JPanel {
         Dimension windowSize = getSize();
 
         boolean widthIsGreater = toDraw.getWidth() >= toDraw.getHeight();
+        
         int largestSide = (widthIsGreater ? toDraw.getWidth() : toDraw.getHeight());
-        double scale = 1;
+        double scaleW = 1;
+        double scaleH = 1;
         if (widthIsGreater) {
-            scale = (double)windowSize.getWidth() / (double)largestSide;
+            scaleW = (double)windowSize.getWidth() / (double)largestSide;
         } else {
-            scale = (double)windowSize.getHeight() / (double)largestSide;
+            scaleH = (double)windowSize.getHeight() / (double)largestSide;
         }
         //System.out.println(PISlideshow.frameSize().width);
 
-        g2.scale(scale, scale);
-        if (widthIsGreater) {
-            int yOffset = (int)((double)((int)windowSize.getHeight() - (int)((double)toDraw.getHeight() * scale)) * 1f/scale) / 2;
-            
-            g2.drawImage(toDraw, null, 0, yOffset);
-        } else {
-            int xOffset = (int)((double)((int)windowSize.getWidth() - (int)((double)toDraw.getWidth() * scale)) * 1f/scale) / 2;
+        double scaleChosen = widthIsGreater ? scaleW : scaleH;
+        boolean imageWider = (int)((double)toDraw.getWidth() * scaleChosen) > windowSize.getWidth();
+        boolean imageTaller = (int)((double)toDraw.getHeight() * scaleChosen) > windowSize.getHeight();
 
-            g2.drawImage(toDraw, null, xOffset, 0);
+        ///g2.scale(scale, scale);
+        int yOffset = 0;
+        int xOffset = 0;
+        if (widthIsGreater) {
+            
+            if (imageTaller) {
+                double modScale = scaleChosen * ((double)windowSize.getHeight() / ((double)toDraw.getHeight() * scaleChosen));
+                // System.out.println("Taller: " + scaleChosen + " -> " + modScale); // confirmed works
+                g2.scale(modScale, modScale);
+                xOffset = (int)((double)((int)windowSize.getWidth() - (int)((double)toDraw.getWidth() * modScale)) * 1f/modScale) / 2;
+            } else {
+                g2.scale(scaleChosen, scaleChosen);
+                yOffset = (int)((double)((int)windowSize.getHeight() - (int)((double)toDraw.getHeight() * scaleChosen)) * 1f/scaleChosen) / 2;
+            }
+        } else {
+            if (imageWider) {
+                double modScale = scaleChosen * ((double)windowSize.getWidth() / ((double)toDraw.getWidth() * scaleChosen));
+                // System.out.println("Wider: " + scaleChosen + " -> " + modScale); // confirmed works
+                g2.scale(modScale, modScale);
+                yOffset = (int)((double)((int)windowSize.getHeight() - (int)((double)toDraw.getHeight() * modScale)) * 1f/modScale) / 2;
+            } else {
+                g2.scale(scaleChosen, scaleChosen);
+                xOffset = (int)((double)((int)windowSize.getWidth() - (int)((double)toDraw.getWidth() * scaleChosen)) * 1f/scaleChosen) / 2;
+            }
         }
+        g2.drawImage(toDraw, null, xOffset, yOffset);
         g2.dispose();
     }
 
